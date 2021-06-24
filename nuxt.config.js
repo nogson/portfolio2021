eef1a0687785578b17bfad9aca080f075b7093f9
佐藤文彦 <fsato@ciaoinc.jp>
2021-06-24 16:52:08 +0900

    no message

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,13 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/portfolio2021/favicon.ico',
+      },
+    ],
   },
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
@@ -77,5 +83,8 @@ export default {
         loader: 'frontmatter-markdown-loader',
       })
     },
+  },
+  router: {
+    base: '/portfolio2021',
   },
 }
