e0256a5eccaf4361746246275d0fc8e0816a3fd2
nogson <nogson@users.noreply.github.com>
2022-01-04 23:51:15 +0000

    deploy: cce313155e38c397dde7e6ae131059ff1227acdc

--- _nuxt/static/1641256120/portfolio/web-service-03/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1641256120",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio\u002Fweb-service-03",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f37ff5f0"}}}}(null));
