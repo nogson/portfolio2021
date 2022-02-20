4dfd8453e81e97429959838af713ebf4a9c96d5f
nogson <nogson@users.noreply.github.com>
2022-02-20 09:04:14 +0000

    deploy: 76cd851cc5c245b0388fcf0a020618fb3b06ad60

--- _nuxt/static/1645264185/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1645264185",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6faa1a4e"}}}}(null,"\u002F"));
