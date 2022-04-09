7f64684cef223d0ec3c66f4120fe26bfaaf9303e
nogson <nogson@users.noreply.github.com>
2022-04-09 00:16:58 +0000

    deploy: e18793d8571d56705aa2d506b7973c4c0370a43c

--- _nuxt/static/1649462680/blog/20220407/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649462680",layout:"content",error:a,serverRendered:true,routePath:"\u002Fblog\u002F20220407",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f0f1afe0"}}}}(null));
---
+++ _nuxt/static/1649463387/portfolio/lp-01/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1649463387",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio\u002Flp-01",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"168929bf"}}}}(null));
