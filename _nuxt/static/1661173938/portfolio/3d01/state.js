9db4315b5a23f255c686f0f203bdd3261f803279
nogson <nogson@users.noreply.github.com>
2022-08-22 13:12:49 +0000

    deploy: 810cfa561516af2c6a58107747e8c46fdfdb7b6c

---
+++ _nuxt/static/1661173938/portfolio/3d01/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661173938",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio\u002F3d01",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7a6e9cf9"}}}}(null));
