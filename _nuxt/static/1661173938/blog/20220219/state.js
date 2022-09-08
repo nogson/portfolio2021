100719d76b67f1ccd584772fecc8f2415cb435c5
nogson <nogson@users.noreply.github.com>
2022-09-08 08:58:43 +0000

    deploy: f36fd06bfef3380b0310e0f0bd7ece83deb119a7

--- _nuxt/static/1661173938/blog/20220219/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661173938",layout:"content",error:a,serverRendered:true,routePath:"\u002Fblog\u002F20220219",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7a6e9cf9"}}}}(null));
