8ba60742fe8f86ac0aff9fa249af0102feae5bbc
nogson <nogson@users.noreply.github.com>
2022-10-05 14:52:09 +0000

    deploy: 9b3aaf397a5c6f66703e4961d6bd7e9d965e4ece

--- _nuxt/static/1663682308/blog/20200805/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663682308",layout:"content",error:a,serverRendered:true,routePath:"\u002Fblog\u002F20200805",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"80b23852"}}}}(null));
---
+++ _nuxt/static/1664981489/portfolio/touchdesigner02/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1664981489",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio\u002Ftouchdesigner02",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f7de6b75"}}}}(null));
