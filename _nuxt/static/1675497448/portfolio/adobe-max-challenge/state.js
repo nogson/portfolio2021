d624d19f29d8fd7d79f9af3559221c8f8ae1e8dc
nogson <nogson@users.noreply.github.com>
2023-02-04 13:56:46 +0000

    deploy: ef02a40b4a0344a9c1a29c405dbea4acdd6e873a

--- _nuxt/static/1675497448/portfolio/adobe-max-challenge/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1675497448",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio\u002Fadobe-max-challenge",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"db66fc1c"}}}}(null));
