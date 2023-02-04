fae878105b29898cad7f8b184733376a5cae17cf
nogson <nogson@users.noreply.github.com>
2023-02-04 14:55:38 +0000

    deploy: b43c48583231c24351a350955de84f2ec991f101

--- _nuxt/static/1675518985/portfolio/touchdesigner02/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1675518985",layout:"default",error:a,serverRendered:true,routePath:"\u002Fportfolio\u002Ftouchdesigner02",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9950472f"}}}}(null));
